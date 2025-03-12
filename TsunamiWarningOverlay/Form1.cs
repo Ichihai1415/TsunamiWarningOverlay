47c08ca7e2b2a8b9861a45f2b1877411c1601419
Ichihai1415 <projects3141.gif@gmail.com>
2025-03-12 17:54:35 +0900

    プロジェクト ファイルを追加します。

---
+++ TsunamiWarningOverlay/Form1.cs
@@ -0,0 +1,26 @@
+using static TsunamiWarningOverlay.Utils;
+
+namespace TsunamiWarningOverlay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public Bitmap Draw(Data data)
+        {
+
+
+        }
+
+    }
+}
+
+
---
+++ TsunamiWarningOverlay/Utils.cs
@@ -0,0 +1,56 @@
+﻿namespace TsunamiWarningOverlay
+{
+    internal static class Utils
+    {
+        public enum TsunamiGrade
+        {
+            /// <summary>
+            /// (発表無し)
+            /// </summary>
+            None = 0,
+            /// <summary>
+            /// 津波予報(jma xmlのみ)
+            /// </summary>
+            Forecast = 1,
+            /// <summary>
+            /// 津波注意報
+            /// </summary>
+            Watch = 2,
+            /// <summary>
+            /// 津波警報
+            /// </summary>
+            Warning = 3,
+            /// <summary>
+            /// 大津波警報
+            /// </summary>
+            MajorWarning = 4
+        }
+
+
+
+
+
+        public class Data
+        {
+            public DateTime ReceiveTime { get; set; } = DateTime.MinValue;
+            public DateTime AnnouncementTime { get; set; } = DateTime.MinValue;
+
+            public AreaData[] AreaDatas { get; set; } = []; 
+
+            public class AreaData
+            {
+                public string AreaName { get; set; } = string.Empty;
+
+                public TsunamiGrade Grade { get; set; }
+
+            }
+
+
+        }
+
+        public class Config
+        {
+
+        }
+    }
+}
